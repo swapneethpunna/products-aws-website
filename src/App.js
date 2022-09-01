3ea2bf078c9b74060f68abbabbc3dc3c1a9b3b7f
swapneethpunna <swapneethpunna@msitprogram.net>
2022-09-01 22:52:51 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data.js';
+import {useState} from 'react'
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand"><b>ZOMATO</b></a>
+          <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Search" onChange={event =>{setSearchTerm(event.target.value)}} aria-label="Search"/>
+          </form>
+        </div>
+      </nav>
+      {/* Container */}
+      <div className='container mt-3 mb-3'>
+        {/* Start of Row */}
+        <div className="row row-cols-1 row-cols-lg-4 g-4">
+          {/* Searc cum rendering data */}
+            {data.filter((val) =>{
+              if (searchTerm === ""){
+                return val
+              } else if (val.productName.toLowerCase().includes(searchTerm.toLowerCase())){
+                return val
+              }
+            }).map((val) =>{
+              return (
+                    <div className="col-md-6 col-lg-4">
+                      <div className="card">
+                        <img src={val.Image} className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                          <h5 className="card-title">{val.productName}</h5>
+                          <p className="card-text">Rs - {val.price}/-</p>
+                        </div>
+                      </div>
+                    </div>
+              )
+            })}
+        </div>  
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/data.js
@@ -0,0 +1,91 @@
+const data=[
+
+    {
+        "id":1,
+        "productName":"pizza",
+        "price": 499,
+        "Image":"./Assests/pizza.jpg",
+    },
+
+    {
+        "id":2,
+        "productName":"burger",
+        "price": 200,
+        "Image":".//Assests/burger.jpg",
+    },
+
+    {
+        "id":3,
+        "productName":"chicken biryani",
+        "price": 180,
+        "Image":"./Assests/chickenbiryani.jpg",
+    },
+
+    {
+        "id":4,
+        "productName":"mutton biryani",
+        "price": 250,
+        "Image":".//Assests/muttonbiryani.jpg",
+    },
+
+    {
+        "id":5,
+        "productName":"chicken tandoori",
+        "price": 250,
+        "Image":"./Assests/chickentandoori.jpg",
+    },
+
+    {
+        "id":6,
+        "productName":"butter naan",
+        "price": 40,
+        "Image":"./Assests/naan.jpg",
+    },
+
+    {
+        "id":7,
+        "productName":"butter chicken",
+        "price": 390,
+        "Image":".//Assests/butterchicken.jpg",
+    },
+
+    {
+        "id":8,
+        "productName":"paneer butter masala",
+        "price": 350,
+        "Image":".//Assests/paneerbuttermasala.jpg",
+    },
+
+   
+
+    {
+        "id":10,
+        "productName":"fruit salad",
+        "price": 120,
+        "Image":"./Assests/fruitsalad.jpg",
+    },
+
+    {
+        "id":11,
+        "productName":"apricot delight",
+        "price": 130,
+        "Image":"./Assests/apricotdelight.jpg",
+    },
+
+    {
+        "id":12,
+        "productName":"chicken shawarma",
+        "price": 120,
+        "Image":"./Assests/shawarma.jpg",
+    },
+
+    {
+        "id":13,
+        "productName":"black forest cake",
+        "price": 550,
+        "Image":"./Assests/blackforest.jpg",
+    },
+
+]
+
+export default data
